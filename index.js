a11b3a400ba08f59638cb0b6b1fa1d4b6fdbe971
Mike Marschall <mike.marschall+github@me.com>
2023-11-21 03:51:04 +0100

    ✨ introduce second version of gallery

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+/*
 import {animate, scroll} from "https://cdn.skypack.dev/motion@10.16.4"
 scroll(animate("#progress-bar", {scaleX: [0, 1]}), {target: "#slides"})
+*/
+
+document.getElementById('controls').classList.add('visible')
+
+const slides = document.getElementById('slides')
+document.getElementById('next-slide').addEventListener('click', function() {
+    slides.scroll({left: slides.scrollLeft + document.body.clientWidth, behavior: 'smooth'})
+})
+document.getElementById('first-slide').addEventListener('click', function() {
+    slides.scroll({left: 0, behavior: 'smooth'})
+})
+slides.addEventListener('scroll', function() {
+    document.getElementById('next-slide').style.opacity = ((slides.scrollLeft + document.body.clientWidth) < slides.scrollWidth-5) ? 1 : 0
+    document.getElementById('first-slide').style.opacity = ((slides.scrollLeft + document.body.clientWidth) < slides.scrollWidth-5) ? 0 : 1
+})
+
+document.getElementById('full-screen').addEventListener('click', function() {
+    document.body.requestFullscreen({navigationUI: "hide"})
+})
+document.addEventListener('fullscreenchange', function() {
+    document.getElementById('full-screen').style.opacity = document.fullscreenElement ? 0 : 1
+})
